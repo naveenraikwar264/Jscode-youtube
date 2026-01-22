b35ddb2434d162373388e7c1492f1b6ec345ed1a
naveenraikwar264 <naveen.raikwar264@gmail.com>
2026-01-22 02:52:55 +0000

    commit

--- 01_basics/03_Convertionoperations.js
+++ 01_basics/03_Convertionoperations.js
@@ -1,73 +1,104 @@
 "use strict"
-// let score ="33"  
+// // let score ="33"  
 
-// console.log(typeof score)
-// console.log(typeof (score))
-// let valuenumber=Number(score)
+// // console.log(typeof score)
+// // console.log(typeof (score))
+// // let valuenumber=Number(score)
 
-// console.log(typeof valuenumber)
-// console.log(typeof (valuenumber))
+// // console.log(typeof valuenumber)
+// // console.log(typeof (valuenumber))
 
-// // --let score ="33abc"  
-// let score2 ="33abc"  
+// // // --let score ="33abc"  
+// // let score2 ="33abc"  
 
-// console.log(typeof score2)
-// console.log(typeof (score2))
-// let valuenumber2=Number(score2)
+// // console.log(typeof score2)
+// // console.log(typeof (score2))
+// // let valuenumber2=Number(score2)
 
-// console.log(typeof valuenumber2)
-// console.log(typeof (valuenumber2))
-// console.log(valuenumber2)
+// // console.log(typeof valuenumber2)
+// // console.log(typeof (valuenumber2))
+// // console.log(valuenumber2)
 
-// // --let score3 =true  
-// let score3 =true  
+// // // --let score3 =true  
+// // let score3 =true  
 
-// console.log(typeof score3)
-// console.log(typeof (score3))
-// let valuenumber3=Number(score3)
+// // console.log(typeof score3)
+// // console.log(typeof (score3))
+// // let valuenumber3=Number(score3)
 
-// console.log(typeof valuenumber3)
-// console.log(typeof (valuenumber3))
+// // console.log(typeof valuenumber3)
+// // console.log(typeof (valuenumber3))
 
-// // --let score4 =false  
-// let score4 =false  
+// // // --let score4 =false  
+// // let score4 =false  
 
-// console.log(typeof score4)
-// console.log(typeof (score4))
-// let valuenumber4=Number(score4)
+// // console.log(typeof score4)
+// // console.log(typeof (score4))
+// // let valuenumber4=Number(score4)
 
-// console.log(typeof valuenumber4)
-// console.log(typeof (valuenumber4))
+// // console.log(typeof valuenumber4)
+// // console.log(typeof (valuenumber4))
 
-// // --let score5 =null  
-// let score5 =null  
+// // // --let score5 =null  
+// // let score5 =null  
 
-// console.log(typeof score5)
-// console.log(typeof (score5))
-// let valuenumber5=Number(score5)
+// // console.log(typeof score5)
+// // console.log(typeof (score5))
+// // let valuenumber5=Number(score5)
 
-// console.log(typeof valuenumber5)
-// console.log(typeof (valuenumber5))
+// // console.log(typeof valuenumber5)
+// // console.log(typeof (valuenumber5))
 
-// // --let score6 =undefined  
-// let score6 =undefined  
+// // // --let score6 =undefined  
+// // let score6 =undefined  
 
-// console.log(typeof score6)
-// console.log(typeof (score6))
-// let valuenumber6=Number(score6)
+// // console.log(typeof score6)
+// // console.log(typeof (score6))
+// // let valuenumber6=Number(score6)
 
-// console.log(typeof valuenumber6)
-// console.log(typeof (valuenumber6))  
-let isloggedin="true"
+// // console.log(typeof valuenumber6)
+// // console.log(typeof (valuenumber6))  
+// let isloggedin="true"
 
-console.log(typeof isloggedin)
-console.log(typeof (isloggedin))
-let booleanvalue=Boolean(isloggedin)
+// console.log(typeof isloggedin)
+// console.log(typeof (isloggedin))
+// let booleanvalue=Boolean(isloggedin)
 
-console.log(typeof booleanvalue)
-console.log(booleanvalue)
-//1=>True ;0=>false
+// console.log(typeof booleanvalue)
+// console.log(booleanvalue)
+// //1=>True ;0=>false
 //""=>false ;"abc"=>true
 //null=>false
 //undefined=>false
-//NaN=>false    
+//NaN=>false
+// let value=3
+// let NegValue=-value
+// console.log(NegValue)
+// console.log(2**3)
+// console.log(2%3)
+// let str1="Hello"
+// let str2="World"
+// console.log(str1+" "+str2)
+// console.log(1+"2")
+// console.log(1+2+"2")
+// console.log(1+"2"+"2")
+// console.log("1"+2+2)
+// console.log(+true)
+// console.log(+false)
+// console.log(+"")
+let gamecounter=100
+gamecounter++
+console.log(gamecounter)
+gamecounter--
+console.log(gamecounter)
+gamecounter+=5
+console.log(gamecounter)
+gamecounter-=3
+console.log(gamecounter)
+gamecounter*=2
+console.log(gamecounter)
+gamecounter/=4
+console.log(gamecounter)
+gamecounter%=6
+console.log(gamecounter)
+//Link to study this topic more:https://tc39.es/ecma262/multipage/abstract-operations.html#sec-toprimitive
