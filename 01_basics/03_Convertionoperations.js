7a0fe8abd3545ddcb0c6830292f65dc90ec90886
naveenraikwar264 <naveen.raikwar264@gmail.com>
2026-01-22 02:27:05 +0000

    convertionoperations

--- 01_basics/03_Convertionoperations.js
+++ 01_basics/03_Convertionoperations.js
@@ -1,5 +1,73 @@
-let score ="33"  
+"use strict"
+// let score ="33"  
 
-console.log(typeof score)
-console.log(typeof (score))
-let valuenumber=Number (score)
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valuenumber=Number(score)
+
+// console.log(typeof valuenumber)
+// console.log(typeof (valuenumber))
+
+// // --let score ="33abc"  
+// let score2 ="33abc"  
+
+// console.log(typeof score2)
+// console.log(typeof (score2))
+// let valuenumber2=Number(score2)
+
+// console.log(typeof valuenumber2)
+// console.log(typeof (valuenumber2))
+// console.log(valuenumber2)
+
+// // --let score3 =true  
+// let score3 =true  
+
+// console.log(typeof score3)
+// console.log(typeof (score3))
+// let valuenumber3=Number(score3)
+
+// console.log(typeof valuenumber3)
+// console.log(typeof (valuenumber3))
+
+// // --let score4 =false  
+// let score4 =false  
+
+// console.log(typeof score4)
+// console.log(typeof (score4))
+// let valuenumber4=Number(score4)
+
+// console.log(typeof valuenumber4)
+// console.log(typeof (valuenumber4))
+
+// // --let score5 =null  
+// let score5 =null  
+
+// console.log(typeof score5)
+// console.log(typeof (score5))
+// let valuenumber5=Number(score5)
+
+// console.log(typeof valuenumber5)
+// console.log(typeof (valuenumber5))
+
+// // --let score6 =undefined  
+// let score6 =undefined  
+
+// console.log(typeof score6)
+// console.log(typeof (score6))
+// let valuenumber6=Number(score6)
+
+// console.log(typeof valuenumber6)
+// console.log(typeof (valuenumber6))  
+let isloggedin="true"
+
+console.log(typeof isloggedin)
+console.log(typeof (isloggedin))
+let booleanvalue=Boolean(isloggedin)
+
+console.log(typeof booleanvalue)
+console.log(booleanvalue)
+//1=>True ;0=>false
+//""=>false ;"abc"=>true
+//null=>false
+//undefined=>false
+//NaN=>false    
