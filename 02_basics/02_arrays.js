e2fe90868973b515f5b2a3421ae68fee5c4b496a
naveenraikwar264 <naveen.raikwar264@gmail.com>
2026-01-24 17:22:56 +0000

    last

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -10,3 +10,5 @@ console.log(all_new_heros)
 const another_array=[1,2,3,[4,5],6,[7,8,[9,10]]]        
 console.log(another_array.flat(2))  //flat method flattens the nested arrays up to the specified depth.     
 //In this case, flat(2) flattens the array up to 2 levels deep.
+console.log(another_array.flat(Infinity)) 
+console.log(Array.isArray(another_array)) //isArray method checks if the given value is an array and returns a boolean value (true or false).
